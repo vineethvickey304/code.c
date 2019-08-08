5f81bb0d35003134fff5a67158ebdfabea02f662
vineethvickey304 <41952459+vineethvickey304@users.noreply.github.com>
2019-08-08 12:46:04 +0530

    Add files via upload

---
+++ code.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+    int a,p,count,ref,b;
+  
+   scanf("%d%d",&x,&p);
+ 
+   b=0;
+   
+   count=0;
+ 
+   while(a>1)
+  
+  {
+      
+  b=b+a;
+    
+    ref=(x*(p/100));
+   
+     a=a-ref;
+      
+  count=count+1;
+    
+  
+     
+    }
+    
+ printf("%d",b);
+    
+
+ 
+    return 0;
+
+}
